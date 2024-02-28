b94659d0f46f2d1624c5693b3e672078efc3d8c9
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-02-28 11:56:17 -0700

    hh

--- src/components/dtilse/Ditilse.jsx
+++ src/components/dtilse/Ditilse.jsx
@@ -3,12 +3,18 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // import Product from "../product/Product";
 import Img from "../lezylodeimg/Img";
+import './Ditilse.css'
+// import BeatLoader from "react-spinners/BeatLoader";
 
 const Ditilse = () => {
+  const [loading, setLoding] = useState(false);
 
-
-
-
+  useEffect(() => {
+    setLoding(true);
+    setTimeout(() => {
+      setLoding(false);
+    }, 500);
+  }, []);
   const prams = useParams();
   console.log(prams);
   const [product, setproduct] = useState([]);
@@ -18,7 +24,7 @@ const Ditilse = () => {
         const products = await axios.get(
           `https://fakestoreapi.com/products/${prams.productId}`
         );
-      setproduct(products.data);
+        setproduct(products.data);
         console.log(product.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -27,32 +33,47 @@ const Ditilse = () => {
 
     fechProduct();
   }, []);
- 
 
   return (
-    <div style={{marginTop:"5rem"}}>
+    <>
+      {loading ? (
+        //     <BeatLoader
+        //    color={"#aaa"}
+        //    loading={loading}
+        //   //  cssOverride={override}
+        //    size={150}
 
-        <div>
-            <Img src={product.image} className="imgDitilse"/>   
-      
-      
-      
-      
-      <h2>Price:  {product.price}$</h2>
-      <h3>Title : {product.title}</h3>
-      <h6>description: {product.description}</h6>
+        //    aria-label="Loading Spinner"
+        //    data-testid="loader"
+        //  />
+        <div className="detailsBannerSkeleton">
+          {/* <ContentWrapper> */}
+          <div className="left skeleton "></div>
+          <div className="right ">
+            <div className="row skeleton"></div>
+            <div className="row skeleton"></div>
+            <div className="row skeleton"></div>
+            <div className="row skeleton"></div>
+            {/* <div className="row skeleton"></div>
+            <div className="row skeleton"></div> */}
+         
+          </div>
+          {/* </ContentWrapper> */}
         </div>
-
-   
-    
-    
-
-   
-  
-
-      {/* <Product product={product} showButton={false} /> */}
-
-    </div>
+      ) : (
+        <div className="ditilse-section" style={{ marginTop: "5rem" }}>
+          <div className="left-img">
+            <Img src={product.image} className="imgDitilse" />
+          </div>
+          <div className="right-info" >
+            <h2>Price: {product.price}$</h2>
+            <h3>Title : {product.title}</h3>
+            <h6>description: {product.description}</h6>
+          </div>
+          {/* <Product product={product} showButton={false} /> */}
+        </div>
+      )}
+    </>
   );
 };
 
