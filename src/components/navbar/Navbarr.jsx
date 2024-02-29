d9c4fc1ebef5304c6ccc55b77b0aa76f98129dc0
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2024-02-29 04:07:18 -0700

    handle nav bar

--- src/components/navbar/Navbarr.jsx
+++ src/components/navbar/Navbarr.jsx
@@ -1,36 +1,63 @@
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./Navbar.css";
+import { CgShoppingCart } from "react-icons/cg";
 
 const Navbarr = () => {
- 
- 
-  const cart = useSelector((state)=>state.cart)
+  const [scrolled, setScroll] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 110) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  // const { cartQuntity } = useShoppingCart();
+
+  const cart = useSelector((state) => state.cart);
   // console.log(cart);
   return (
     <>
-<Navbar fixed="top"   expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#">Store</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="/cart">Cart{cart.length}</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <div
+        id="Navbar"
+        className={`contanerr ${scrolled ? "stiky-header" : ""}`}
+      >
+        <div className="center">
+          <a href="#" style={{ textDecoration: "none", color: "#fff" }}>
+            <span>Sh Store</span>
+          </a>
+        </div>
 
-
-
-
-
+        <div className="right-nav">
+          {/* <AiOutlineSearch className='icon'/> */}
+          {/* <AiOutlineHeart  className='icon'/> */}
+          <div className="cart-nav">
+            <Link style={{ textDecoration: "none", color: "#fff" }} to="/">
+              Home
+            </Link>
+            <Link
+              style={{ textDecoration: "none", color: "#fff" }}
+              className="icon"
+              to="/cart"
+            >
+              <CgShoppingCart />
+              {cart.length}
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbarr
+export default Navbarr;
